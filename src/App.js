34d6a57bb1aac64f246753490bb850818138b036
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2022-06-03 01:05:40 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+
+const [myCards, setMyCards] = useState([])
+
+useEffect(() => {
+  setMyCards([
+    '/imgs/Cool.png',
+    '/imgs/Bomba.png',
+    '/imgs/Macka 3.png',
+    '/imgs/Macka 5.png',
+    '/imgs/Zmesaj.png',
+  ])
+}, [])
+
+
+const renderMyCards = () => {
+  return(
+    <>
+    {
+      myCards.map(card => <img src={card} alt={card}/>)
+    }
+    </>
+  )
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className="container">
+        <h1>Macke</h1>
+        <div className="main-cards">
+          <img src="/imgs/Cool.png" alt="" />
+        </div>
+        <div className="players">
+          <div className='player'>player1</div>
+          <div className='player'>player2</div>
+          <div className='player'>
+            {
+              renderMyCards()
+            }
+            player3
+            </div>
+          <div className='player'>player4</div>
+        </div>
+      </div> 
     </div>
   );
 }
